9976ee9563df321e1137057655d4f598394cc10f
sk-shakeel <shaik.shakeel@zemosolabs.com>
2023-06-23 11:09:50 +0530

    added Interface to follow ISP

--- src/main/java/firehazard/LightCounter.java
+++ src/main/java/firehazard/LightCounter.java
@@ -1,15 +1,17 @@
 package firehazard;
 
-public class LightCounter extends LightOperator{
+public class LightCounter implements Counter{
+
+    @Override
     public int count(LightsGrid lightsGrid) {
-        int litCount = 0;
-        for (int x = 0; x < lightsGrid.lights.length; x++) {
-            for (int y = 0; y < lightsGrid.lights[x].length; y++) {
-                if (lightsGrid.lights[x][y].getState()) {
-                    litCount++;
+            int litCount = 0;
+            for (int x = 0; x < lightsGrid.lights.length; x++) {
+                for (int y = 0; y < lightsGrid.lights[x].length; y++) {
+                    if (lightsGrid.lights[x][y].getState()) {
+                        litCount++;
+                    }
                 }
             }
+            return litCount;
         }
-        return litCount;
-    }
 }
